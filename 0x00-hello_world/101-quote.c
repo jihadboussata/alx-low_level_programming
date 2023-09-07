0379df3c1a57af1cba08df96ac4c56a25bc025c4
jihadboussata <jihadboussata@gmail.com>
2023-09-07 16:07:37 -0700

    fixing 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
 /**
  * main - main function
  *
- * Result: 1
+ * Return: It is 1
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+
 	return (1);
 }

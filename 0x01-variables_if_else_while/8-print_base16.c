341c42114074c3bb5eb38062c909b57f3799bf3c
jihadboussata <jihadboussata@gmail.com>
2023-09-08 16:43:35 -0700

    add 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	char j;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (j = 'a', j <= 'f'; j++)
+		putchar(i)
+
+	putchar('\n');
+
+	return (0);
+
+}

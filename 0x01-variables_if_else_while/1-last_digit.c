8dc2c428dc8099910862e81f7f873decba1418f0
jihadboussata <jihadboussata@gmail.com>
2023-09-08 07:18:09 -0700

    0x01 handling..

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/*
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n, m;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	/* Checking the sign of n */
+
+	m = n % 10;
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, m);
+	else if (m == 0)
+		printf("Last figit of %d is %d and is zero", n, m);
+	else if (m < 6 && m != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0");
+
+
+	printf("\n");
+
+	return (0);
+}

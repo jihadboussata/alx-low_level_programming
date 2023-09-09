368c4e8321adc383ea063c2fba640f5fabfa26f1
jihadboussata <jihadboussata@gmail.com>
2023-09-08 17:44:12 -0700

    ..

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -25,15 +25,15 @@ int main(void)
 			putchar(i + '0');
 			putchar(j + '0');
 			
-		if (i < 8 || j < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
